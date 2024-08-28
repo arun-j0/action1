a75de6a02e2263cc9ee73e84289d3979ef66e5db
arun-j0 <arun2310kumar2002@gmail.com>
2024-08-28 14:04:44 +0530

    main1 update

--- main1.py
+++ main1.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
     message = sys.argv[1] if len(sys.argv) > 1 else "No input"
-    print(message)
+    print(message + " (program)")
     
 if __name__ == "__main__":
     main()
